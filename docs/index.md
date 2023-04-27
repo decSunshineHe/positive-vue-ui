965bced70c8c153551b597ddcaef498454395654
何文杰 <hewenjie@hewenjiedeMacBook-Pro.local>
2023-04-27 18:28:00 +0800

    init project

---
+++ docs/index.md
@@ -0,0 +1,8 @@
+# What is My Lib?
+
+This library is a starter to create your own Vue 3 component library in TypeScript.
+
+There are two example components included for you to get started with creating your own components:
+
+- [ComponentA](/components/component-a) is a simple Hello World component with icon from bundled font file.
+- [ComponentB](/components/component-b) is a counter component using
---
+++ src/components/ComponentB.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <div class="flex align-content-center flex-wrap counter">
+    <label class="flex align-items-center justify-content-center"
+      >Counter:</label
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.counter {
+  label {
+    padding-right: 10px;
+    font-weight: bold;
+  }
+  .count {
+    padding: 0 10px;
+  }
+}
+</style>
