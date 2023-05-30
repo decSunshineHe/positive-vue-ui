1daecd6c4d916cde99e036f263dbea40f5d5cd51
decSunshineHe <1136031918@qq.com>
2023-05-30 18:04:27 +0800

    feat: 文档构建

---
+++ build-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -xe
+
+COMMAND=$1
+
+PROJECT_NAME=$(echo 'console.log(require("./package.json").name)' | node -)
+VERSION=$(git describe --tag --dirty --long)
+DOCKER_IMAGE_REPO=192.168.2.106:8680/demo
+SERVER_ADDR=root@192.168.2.101
+
+IMAGE_NAME=$DOCKER_IMAGE_REPO/$PROJECT_NAME:$VERSION
+
+case $COMMAND in
+build)
+    npm ci
+    npm run docs:build
+    docker image build ./ -t "$IMAGE_NAME"
+    ;;
+push)
+    docker push "$IMAGE_NAME"
+    ;;
+deploy)
+    ssh "$SERVER_ADDR" kubectl set image deployment "$PROJECT_NAME" vue-front-container="$IMAGE_NAME" -n dev
+    ;;
+esac
