30b558c910b1e60e5f4f4f417cfdce9528904c9d
hewenjie <1136031918@qq.com>
2023-10-10 14:57:14 +0800

    feat: 发布脚本

--- release.js
+++ release.js
@@ -4,20 +4,30 @@
 'use strict';
 
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const spawn = require('child_process').spawnSync;
+const childProcess = require('child_process');
+const chalk = require('chalk');
 
+const exec = util.promisify(childProcess.exec);
+const spawn = childProcess.spawnSync;
+
+const error = chalk.bold.red;
+const info = chalk.keyword('green');
+
+// 版本控制
 async function version(versionType, tagType) {
   const { stdout, stderr } = await exec(
     `npm version ${versionType} ${tagType ? `--preid=${tagType}` : ''} --no-git-tag-version`
   );
   if (stderr) throw stderr;
+  console.log(info(`版本升级到：${stdout.trim()}`));
   return stdout;
 }
 
+// 分支管理
 async function branch() {
   const { stdout, stderr } = await exec(`git rev-parse --abbrev-ref HEAD`);
   if (stderr) throw stderr;
+  console.log(info(`当前发版分支为：${stdout.trim()}`));
   return stdout;
 }
 
@@ -26,33 +36,37 @@ const run = async () => {
     const versionType = process.argv[2];
     const tagType = process.argv[3];
 
-    if (versionType !== 'patch' && versionType !== 'minor' && versionType !== 'major' && versionType !== 'prerelease')
-      throw new Error('You need to specify npm version! [patch|minor|major]');
+    const latVersions = ['patch', 'minor', 'major'];
+    const preVersions = ['prepatch', 'preminor', 'premajor', 'prerelease'];
 
     // 当前分支
     const currentBranch = await branch();
-    if (tagType === 'beta' && currentBranch !== 'test') {
-      throw new Error('publish beta please checkout branch to test');
+
+    // test分支限定发布beta
+    if (preVersions.includes(versionType) && tagType && currentBranch.trim() !== 'test') {
+      throw new Error('发布beta版本请切换到test分支！');
     }
-    if (currentBranch != 'main') {
-      throw new Error('publish latest please checkout branch to main');
+    // release/v0分支限定发布latest
+    if (latVersions.includes(versionType) && !tagType && currentBranch.trim() != 'main') {
+      throw new Error('发布稳定版本请切换到main分支！');
     }
 
     const npmVersion = await version(versionType, tagType);
 
     await spawn('git', ['add', 'package.json', 'package-lock.json'], { stdio: 'inherit' });
-    await spawn('git', ['commit', '-m', gitMessage.trim()], { stdio: 'inherit' });
+    await spawn('git', ['commit', '-m', npmVersion.trim()], { stdio: 'inherit' });
+    await spawn('git', ['status'], { stdio: 'inherit' });
+    await spawn('git', ['push', 'origin', currentBranch.trim()], { stdio: 'inherit' });
+
     // 发布稳定版才进行标签
-    if (['patch', 'minor', 'major'].some(versionType)) {
+    if (latVersions.includes(versionType)) {
       await spawn('git', ['tag', npmVersion.trim()], { stdio: 'inherit' });
     }
-    await spawn('git', ['status'], { stdio: 'inherit' });
 
-    await spawn('git', ['push', 'origin', currentBranch.trim()], { stdio: 'inherit' });
+    // await publish(tagType);
   } catch (err) {
-    console.log('Something went wrong:');
-    console.error(err.message);
-    console.error('\nPlease use this format: \nnpm run commit [patch|minor|major] "Commit message"');
+    console.log(error(`出错了： ${err.message}`));
+    process.exit(1);
   }
 };
 
